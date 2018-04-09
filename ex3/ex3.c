baefd6699ba6f18250bf02996236fa2e4deb323d
{theMultitude} <{os.schroeder@gmail.com}>
2018-04-09 17:58:28 -0500

    added and fixed waitpid

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,18 +5,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     int rc = fork();
     if (rc < 0) {
-      fprintf(stderr, 'fork versagt\n');
+      fprintf(stderr, "fork versagt\n");
       exit(1);
     } else if (rc == 0) {
-      printf("hello")
+      printf("guten tag!\n");
     } else {
       int wc = waitpid(rc, NULL, 0);
-      printf("goodbye")
+      printf("aufwiedersehen!\n");
     }
 
     return 0;
